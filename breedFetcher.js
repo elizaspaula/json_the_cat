41173500364b7ff64a3460a7924aa442becf7456
Elizabete Silva de Paula <elizabete.silvapaula@gmail.com>
2021-09-26 02:51:01 +0000

    Created the callback function printOutCataBreeder

--- breedFetcher.js
+++ breedFetcher.js
@@ -2,21 +2,26 @@ const breeder = process.argv[2];
 
 const request = require("request");
 
-const fetchBreederDescription = (breeder, callback) => {
+const fetchBreedDescription = (breeder, callback) => {
   request(
     "https://api.thecatapi.com/v1/breeds/search?q=" + breeder,
     (error, response, body) => {
       if (error) {
-        return callback(error);
+        callback(error);
       } else {
         const data = JSON.parse(body);
-        if (data.lenght === 0) {
-          return callback("It's a breeder name");
+        if (data.length === 0) {
+          callback("It's not a breeder name");
         } else {
-          return data[0].description;
+          callback(data[0].description);
         }
       }
     }
   );
 };
-console.log(fetchBreedDescription("Maine Coon"));
+
+const printOutCatBreeder = (breeder) => {
+  console.log("Return value:", breeder);
+};
+
+fetchBreedDescription(breeder, printOutCatBreeder);
